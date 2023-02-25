a4033ec6a3b4e0f25421498f50f720bba7bb1d34
Jean Jonathan <jeanjonathank2@gmail.com>
2023-02-25 08:45:37 +0100

    Configuration de l environnement de developpement spring boot pret page d acceuil ok sur le serveur localhost 8080

---
+++ src/main/java/com/example/bibliothequeApp/BibliothequeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibliothequeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothequeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliothequeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bibliothequeApp/BibliothequeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibliothequeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
